d4499bfb9de8e7e0236b9b10a08dac74ff897a85
Miguel Chavez <toonchavez8@gmail.com>
2023-12-07 22:36:45 -0600

    fix: change to module and change sends

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,11 +1,11 @@
-const express = require("express");
+import express from "express";
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => {
-	res.send("Hello from the backend!");
+	res.send("Hello from the backend! setup with railway");
 });
 
 app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port} setup for railway`);
 });
