86b65522e37d11a4c68dda4360f79335f38403e0
Miguel Chavez <toonchavez8@gmail.com>
2023-12-09 08:07:56 -0600

    test: testing branch deploy

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -35,7 +35,7 @@ function App() {
 					<img src={reactLogo} className="logo react" alt="React logo" />
 				</a>
 			</div>
-			<h1>DevBranch</h1>
+			<h1>DevFront</h1>
 			<div className="card">
 				<button onClick={() => setCount((count) => count + 1)}>
 					count is {count}
