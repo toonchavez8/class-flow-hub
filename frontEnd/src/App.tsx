297d33a2c51657386e0374618a1690c2777f7227
Miguel Chavez <toonchavez8@gmail.com>
2023-12-09 08:22:42 -0600

    feat: add tailwind

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -26,18 +26,13 @@ function App() {
 	};
 
 	return (
-		<>
-			<div>
-				<a href="https://vitejs.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>DevFront</h1>
+		<section className="prose lg:prose-xl w-full max-w-2xl mx-auto transition-all duration-500">
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
 			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
+				<button
+					onClick={() => setCount((count) => count + 1)}
+					className="btn btn-seondary"
+				>
 					count is {count}
 				</button>
 				<button onClick={fetchData}>Fetch Data</button>
@@ -49,7 +44,7 @@ function App() {
 			<p className="read-the-docs">
 				Click on the Vite and React logos to learn more
 			</p>
-		</>
+		</section>
 	);
 }
 
