f3b7bfd57a23c45428c53510bc8d5bf0df5d6150
Miguel Chavez <toonchavez8@gmail.com>
2024-01-05 21:59:40 -0600

    Feat: Add Email Compoment

---
+++ frontend/src/components/atoms/EmailForm.tsx
@@ -0,0 +1,24 @@
+const EmailForm = () => {
+	return (
+		<form className="w-full debug">
+			<label className="form-control w-full max-w-xs">
+				<div className="label">
+					<span className="label-text">What is your name?</span>
+					<span className="label-text-alt">Top Right label</span>
+				</div>
+				<input
+					type="text"
+					placeholder="Type here"
+					className="input input-bordered w-full max-w-xs"
+				/>
+				<div className="label">
+					<span className="label-text-alt">Bottom Left label</span>
+					<span className="label-text-alt">Bottom Right label</span>
+				</div>
+			</label>
+			<button className="btn btn-accent mb-48">Sign Up</button>
+		</form>
+	);
+};
+
+export default EmailForm;
