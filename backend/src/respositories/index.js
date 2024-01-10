7a87ae72a6379828fe5f679f04e7d87c2be4e335
Miguel Chavez <toonchavez8@gmail.com>
2024-01-09 19:49:03 -0600

    Fix: Add comments and status to api routes

--- backend/src/respositories/index.js
+++ backend/src/respositories/index.js
@@ -1,6 +1,8 @@
 import { SubscriptionDAO } from "../dao/persistanceFactory.js";
 import SubscriptionRepository from "./susbscription.respository.js";
 
+// Create an instance of SubscriptionRepository using SubscriptionDAO
 export const subscriptionService = new SubscriptionRepository(
 	new SubscriptionDAO()
 );
+// This service acts as an abstraction for handling subscription data.
--- backend/src/utils/isValidEmail.js
+++ backend/src/utils/isValidEmail.js
@@ -1,4 +1,4 @@
-// Validate email format (example: a simple email format validation)
+// Function to check if email is valid based on regex
 export default function isValidEmailFormat(email) {
 	const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 	return emailRegex.test(email);
