fcc2989dde4a8d631ebe9e4a89d0e28ae6d44ff5
Miguel Chavez <toonchavez8@gmail.com>
2024-06-21 19:31:53 -0600

    feat: Enhance create method in SubscriptionMongoDAO class

--- backend/src/dao/mongo/subscription.mongo.dao.js
+++ backend/src/dao/mongo/subscription.mongo.dao.js
@@ -5,5 +5,14 @@ export default class SubscriptionMongoDAO {
 		this.collection = SubscriptionModel;
 	}
 	getAll = async () => await this.collection.find();
-	create = async (email) => await this.collection.create({ email });
+	create = async (email) => {
+		const existingSubscription = await this.collection.findOne({ email });
+
+		if (existingSubscription) {
+			return { status: 409, message: "Email already exists in the database." };
+		}
+
+		const newSubscription = await this.collection.create({ email });
+		return newSubscription;
+	};
 }
